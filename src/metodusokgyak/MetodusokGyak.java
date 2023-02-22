52d2e886fa14eb920ca61315e78cd5a1bf50bba0
PetiDominik <petidomi05@gmail.com>
2023-02-22 12:44:54 +0100

    4szam osszeadas, 3 szam osszeg gyok javitas

--- src/metodusokgyak/MetodusokGyak.java
+++ src/metodusokgyak/MetodusokGyak.java
@@ -4,19 +4,25 @@ package metodusokgyak;
 public class MetodusokGyak {
 
     public static void main(String[] args) {
-        int a = 1;
-        int b = 4;
+        int a, b, c, d;
+        a = 1;
+        b = 4;
         int elso10Osszeg = elso10SzamOsszege();
         kiir(String.format("Az első 10 szám összege: %d", elso10Osszeg));
         int aBOsszeg = osszead(a, b);
         kiir(String.format("%d + %d = %d", a, b, aBOsszeg));
         
         //írjuk ki 4 szám összegét
-        int[] szamok = {3, 2, 1, 3};
-        kiir(String.format("%d + %d + %d + %d = %d", szamok[0], szamok[1], szamok[2], szamok[3], osszead2(szamok)));
+        c = 4;
+        d = 6;
+        int osszeg = osszead(a, b);
+        osszeg += osszead(c, d);
+        kiir(String.format("%d + %d + %d + %d = %d", a, b, c, d, osszeg));
+        
         //írjuk ki 3 szám összegének a gyökét
-        int c = 4;
-        kiir("sqrt(%d + %d + %d) = %.2f".formatted(a, b, c, haromszamOsszegGyok(a, b, c)));
+        osszeg = osszead(a, b);
+        osszeg = osszead(osszeg, c);
+        kiir("sqrt(%d + %d + %d) = %.2f".formatted(a, b, c, Math.sqrt(osszeg)));
     }
     
     private static int elso10SzamOsszege() {
@@ -28,21 +34,10 @@ public class MetodusokGyak {
         return osszeg;
     }
     
-    private static double haromszamOsszegGyok(int a, int b, int c) {
-        return Math.sqrt(a + b + c);
-    }
-    
     private static int osszead(int a, int b) {
         return a+b;
     }
-    
-    private static int osszead2(int[] szamok) {
-        int osszeg = 0;
-        for (int szam:szamok) {
-            osszeg += szam;
-        }
-        return osszeg;
-    }
+
     
     private static void kiir(String szoveg) {
         System.out.println(szoveg);
